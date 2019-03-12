a4b06afd701a99bcabc76a21e60529931ab01738
Tomty Tomty <thomas.wirth.2@gmail.com>
2019-03-12 18:21:20 +0100

    Bubble Chat Message Design, Chat Message View Model, Converter

---
+++ Fasetto Word/ValueConverters/SentByMeToAlignementConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Fasetto_Word
+{
+    /// <summary>
+    /// A converter that takes in a boolean if a message was sent by me, and aligns to the right
+    /// if not sent by me, align to the left
+    /// </summary>
+    public class SentByMeToAlignementConverter : BasevalueConverter<SentByMeToAlignementConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Fasetto.Word.Core/ViewModels/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// A view model for a chat message thread list
+    /// </summary>
+    public class ChatMessageListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat thread items for the list
+        /// </summary>
+        public List<ChatMessageListItemViewModel> Items { get; set; }
+        
+    }
+
+}
