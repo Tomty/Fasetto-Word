dd0ed43d5e2e2d8ab3c45b49a77c2b910251b81a
Tomty Tomty <thomas.wirth.2@gmail.com>
2019-04-19 14:18:02 +0200

    Attached property for cell padding

---
+++ Fasetto Word/AttachedProperties/PanelChildMarginProperty.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Fasetto_Word
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class PanelChildMarginProperty : BaseAttachedProperty<PanelChildMarginProperty, string>
+	{
+		public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+		{
+			// Get the panel (grid typically)
+			var panel = (sender as Panel);
+
+			// Wait for panel to load
+			panel.Loaded += (s, ee) =>
+			{
+				// Loop each child
+				foreach (var child in panel.Children)
+				{
+					// set it's margin to the given value
+					(child as FrameworkElement).Margin = (Thickness)(new ThicknessConverter().ConvertFromString(e.NewValue as string));
+				}
+			};
+		}
+
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public PanelChildMarginProperty()
+		{
+
+		} 
+		
+		#endregion
+	}
+}
